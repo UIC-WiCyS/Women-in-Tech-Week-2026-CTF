aba17dc9aba77f50bc5202e9121883f55edc099f
acama5 <acama@uic.edu>
2026-09-15 00:28:57 -0500

    implemented the first layer to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,21 @@ import styles from "./About.module.css";
 export default function About() {
 
     return (
-        <div>
-            
+        <div className="about">
+
+            <section className={styles.aboutBody}>
+                <h1 className={styles.question}>What is </h1>
+                <h1 className={styles.question1}> Women in Tech Week?</h1>
+                <p className={styles.answer}>hduiwahiud iuefhaiue hiuhfau hiual hsauhd usahd 
+                    iusadh iusad hiuwakfdkjf  dasdasd asd asd sad sh th rgg hkhk hiu hiu h jhk 
+                    iu h khui tbu5b65ecv bo78n bt6nu uybtyuiu  98n y8ym87iu  hkjashf iaksdhkjsadh
+                     kjsahdkhka sdas r gadf dsf fweawdadsa
+                </p> 
+            </section>
+        
+            <h2 className={styles.aboutFooter}>Made with &lt;3 by WiCyS & WiCS</h2>
+
         </div>
+        
     );
 }
