d161600b8bd0af158612f5d980684094874cfd0e
acama5 <acama@uic.edu>
2026-09-18 03:08:59 -0500

    foundation for challengesOverview and challengeCard

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -1,10 +1,13 @@
 import styles from "./ChallengeCard.module.css";
 
-export default function ChallengeCard({name, points, category, solved}) {
+export default function ChallengeCard({onClick, name, points, category, solved}) {
 
     return (
-        <div>
-
+        <div onClick={onClick} className={styles.challenge}>
+            <h1 className={styles.name}> {name} </h1>
+            <h1 className={styles.points}> {points}</h1>
+            <h1 className={styles.category}> {category}</h1>
         </div>
+
     );
 }
--- src/pages/ChallengesOverview.jsx
+++ src/pages/ChallengesOverview.jsx
@@ -5,15 +5,27 @@ import ChallengeModal from "../components/ChallengeModal";
 import { get_all_challenges } from "../api/firebase_manager";
 
 export default function ChallengesOverview({session}) {
-    const [challenges, setChallenges] = useState([]);
+    const [challenges, setChallenges] = useState([
+        { id: "1", name: "Ada’s Secret Note", category: "x", points: 100, prompt: "xxx..." },
+        { id: "2", name: "Centaur Coordinates", category: "x", points: 125, prompt: "xxx..." },
+        { id: "3", name: "Hidden Frequency", category: "x", points: 100, prompt: "xxx..." },
+        { id: "4", name: "WiCyS Steganography/XOR", category: "x", points: 150, prompt: "xxx..." },
+        { id: "5", name: "Karen Spärck Jones", category: "x", points: 125, prompt: "xxx..." },
+        { id: "6", name: "Hidden Coordinates", category: "x", points: 125, prompt: "xxx..." },
+        { id: "7", name: "Katherine’s Launch Code", category: "x", points: 125, prompt: "xxx..." },
+        { id: "8", name: "Model Verification", category: "x", points: 150, prompt: "xxx..." }
+    ]);
     const [completed, setCompleted] = useState([]);
-    const [openModal, setModal] = useState(false);
+    //starts as false and later true once clicked
+    const [openModal, setModal] = useState(false); 
+    const [selectedChallenge, setSelectedChallenge] = useState(null);
 
     useEffect(() => {    
         // check if session exists
         // fetch completed challenges
         const fetch_challenges = async () => {
             var data = await get_all_challenges();
+            console.log("testing this:", data);
             if (data != null)
             {
                 // update
@@ -33,13 +45,32 @@ export default function ChallengesOverview({session}) {
             <p>Challenges Overview</p>
             {/* TODO: create a map here */}
             {/* replace the [0] with the index of the map element */}
-            { challenges[0] &&
-                <ChallengeCard name={challenges[0].name} points={challenges[0].points} category={challenges[0].category} solved={true}/>
-            }
-                {/* // if a challenge is clicked on, set setmodal to true */}
-                {challenges[0] && openModal &&
-                    <ChallengeModal session={session} challenge={challenges[0]} />
-                }
+            
+            <div className={styles.challengeMap}> 
+                {challenges.map((challenge) => (
+                    <ChallengeCard key={challenge.id} name={challenge.name} points={challenge.points} category={challenge.category} solved={completed.includes(challenge.id)
+                }  
+                    //Todo: add the lock function
+                    onClick={() => {
+                        setSelectedChallenge(challenge);
+                        setModal(true);
+                    }}
+                
+                    />
+            
+                ))}
+            </div>
+            
+            {/*check the boolean state && check there is a selected challenge && get the challenge*/}
+            {openModal && selectedChallenge &&(
+                <div className={styles.overview}>
+                    <div className={styles.singleChallenge}>
+                        <ChallengeModal session={session} challenge={selectedChallenge} />
+                    <button onClick={() => setModal(false)} className={styles.close}>Close</button>
+        </div>
+    </div>
+               
+            )}
             
             {/* <3 */}
             <div className='absolute bottom-0 text-lg text-white text-center justify-self-center font-pixel'>
